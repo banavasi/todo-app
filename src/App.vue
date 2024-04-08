49a1b8f27a14a925d4a53bfbc3f3cae71a4aa551
banavasi <shashank.v.shandilya@gmail.com>
2024-04-08 00:24:31 -0700

    fix(completed assignment 1):

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,10 @@
   <!-- section input -->
   <section class="lg:w-5/12 w-11/12 mx-auto mt-[-6rem]">
     <div class="flex justify-end">
-      <ThemeSwitcher class="my-4" />
+      <div class="flex justify-between w-full items-center">
+        <h1 class="text-4xl font-extrabold">TODO APP</h1>
+        <ThemeSwitcher class="my-4" />
+      </div>
     </div>
     <form
       class="bg-base-300 opacity-80 rounded-sm h-11"
@@ -36,50 +39,98 @@
     class="todos lg:w-5/12 w-11/12 mx-auto relative z-10 opacity-90 mt-2"
   >
     <ul class="bg-base-300 rounded-sm">
-      <li
-        v-for="todo in store.tasks"
-        @click="store.toggleTask(todo.id)"
-        class="py-2 px-3 flex items-center justify-between border-base-content [&:not(:nth-child(1))]:border-t"
-      >
-        <div class="content">
-          <input
-            type="checkbox"
-            :checked="todo.completed"
-            class="checkbox checkbox-xs mr-3"
-          />
-          <span>{{ todo.name }}</span>
-        </div>
-        <button class="text-xs opacity-80">
-          <img src="./assets/img/icon-cross.svg" alt="" />
-        </button>
-      </li>
+      <template v-if="allTasks.length <= 0">
+        <li
+          class="py-2 px-3 flex items-center justify-between border-base-content text-center"
+        >
+          <span class="text-xs opacity-60">No task available</span>
+        </li>
+      </template>
+      <template v-else>
+        <li
+          v-for="todo in allTasks"
+          @click="store.toggleTask(todo.id)"
+          class="py-2 px-3 flex items-center justify-between border-base-content [&:not(:nth-child(1))]:border-t"
+        >
+          <div class="content">
+            <input
+              type="checkbox"
+              :checked="todo.completed"
+              class="checkbox checkbox-xs mr-3"
+            />
+            <span>{{ todo.name }}</span>
+          </div>
+          <button class="text-xs opacity-80">
+            <img src="./assets/img/icon-cross.svg" alt="" />
+          </button>
+        </li>
+      </template>
     </ul>
     <ul
       class="flex items-center lg:justify-around justify-between bg-base-300 px-4 py-1 rounded-sm border-t border-base-content"
     >
       <li>
-        <button class="text-xs opacity-60">3 items left</button>
+        <span class="text-xs opacity-60">{{ store.tasksLeft }} items left</span>
       </li>
       <li class="lg:block hidden">
-        <button class="text-xs opacity-60">All</button>
-        <button class="text-xs opacity-60 px-3">Active</button>
-        <button class="text-xs opacity-60">Completed</button>
+        <button
+          class="text-xs opacity-60"
+          @click="showAll"
+          :class="{ 'text-primary': activeTab === 'all' }"
+        >
+          All
+        </button>
+        <button
+          class="text-xs opacity-60 px-5"
+          @click="showActive"
+          :class="{ 'text-primary': activeTab === 'active' }"
+        >
+          Active
+        </button>
+        <button
+          class="text-xs opacity-60"
+          @click="showCompleted"
+          :class="{ 'text-primary': activeTab === 'completed' }"
+        >
+          Completed
+        </button>
       </li>
       <li>
-        <button class="text-xs opacity-60">Clear Completed</button>
+        <button class="text-xs opacity-60" @click="handleClearCompleted">
+          Clear Completed
+        </button>
       </li>
     </ul>
     <div
       class="lg:hidden bg-base-300 rounded-sm mt-2 py-2 px-3 flex justify-around"
     >
-      <button class="text-xs opacity-60">All</button>
-      <button class="text-xs opacity-60">Active</button>
-      <button class="text-xs opacity-60">Completed</button>
+      <button
+        class="text-xs opacity-60"
+        @click="showAll"
+        :class="{ 'text-primary': activeTab === 'all' }"
+      >
+        All
+      </button>
+      <button
+        class="text-xs opacity-60"
+        @click="showActive"
+        :class="{ 'text-primary': activeTab === 'active' }"
+      >
+        Active
+      </button>
+      <button
+        class="text-xs opacity-60"
+        @click="showCompleted"
+        :class="{ 'text-primary': activeTab === 'completed' }"
+      >
+        Completed
+      </button>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+type TaskTab = "all" | "active" | "completed";
 import { useTaskStore } from "./store/task";
 import ThemeSwitcher from "./components/ThemeSwitcher.vue";
 import { useDarkMode } from "./composables/useTheme";
@@ -87,6 +138,9 @@ import { ref } from "vue";
 const store = useTaskStore();
 const { isDarkMode } = useDarkMode();
 const newTask = ref<string>("");
+
+const allTasks = ref(store.tasks);
+const activeTab = ref<TaskTab>("all");
 const handleNewTask = () => {
   store.addTask({
     id: Math.floor(Math.random() * 1000),
@@ -94,6 +148,23 @@ const handleNewTask = () => {
     completed: false,
   });
   newTask.value = "";
+};
+
+const showAll = () => {
+  allTasks.value = store.showAll;
+  activeTab.value = "all";
+};
+const showActive = () => {
+  allTasks.value = store.showActive;
+  activeTab.value = "active";
+};
+const showCompleted = () => {
+  allTasks.value = store.showCompleted;
+  activeTab.value = "completed";
+};
+const handleClearCompleted = () => {
+  store.clearCompleted();
+  allTasks.value = store.tasks;
 };
 </script>
 
--- src/store/task.ts
+++ src/store/task.ts
@@ -1,24 +1,54 @@
 import { defineStore } from "pinia";
-
+import { ref, computed } from "vue";
 interface Task {
   id: number;
   name: string;
   completed: boolean;
 }
 
-export const useTaskStore = defineStore("task", {
-  state: () => ({
-    tasks: [] as Task[],
-  }),
-  actions: {
-    addTask(task: Task) {
-      this.tasks.push(task);
-    },
-    toggleTask(id: number) {
-      const taskIndex = this.tasks.findIndex((task) => task.id === id);
-      if (taskIndex !== -1) {
-        this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+export const useTaskStore = defineStore(
+  "task",
+  () => {
+    const tasks = ref<Task[]>([]);
+    const addTask = (task: Task) => {
+      tasks.value.push(task);
+    };
+    const removeTask = (id: number) => {
+      tasks.value = tasks.value.filter((task) => task.id !== id);
+    };
+    const showCompleted = computed(() =>
+      tasks.value.filter((task) => task.completed)
+    );
+
+    const showAll = computed(() => tasks.value);
+
+    const showActive = computed(() =>
+      tasks.value.filter((task) => !task.completed)
+    );
+    const clearCompleted = () => {
+      tasks.value = tasks.value.filter((task) => !task.completed);
+    };
+    const toggleTask = (id: number) => {
+      const task = tasks.value.find((task) => task.id === id);
+      if (task) {
+        task.completed = !task.completed;
       }
-    },
+    };
+    // show non completed tasks
+    const tasksLeft = computed(
+      () => tasks.value.filter((task) => !task.completed).length
+    );
+    return {
+      tasks,
+      addTask,
+      removeTask,
+      showCompleted,
+      showAll,
+      showActive,
+      clearCompleted,
+      toggleTask,
+      tasksLeft,
+    };
   },
-});
+  { persist: true }
+);
